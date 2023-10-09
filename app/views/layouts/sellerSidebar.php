f81750ce59cdc6d7e19ac6f411f2700fb15faf4c
Santhush Fernando <2021is028@stu.ucsc.cmb.ac.lk>
2023-10-09 19:02:45 +0530

    add my layouts

---
+++ app/views/layouts/sellerSidebar.php
@@ -0,0 +1,18 @@
+<div class="Sellersidebar">
+    <div class="profile-logo">
+        <label for="image-upload" class="image-label">
+            <img src="<?php echo URLROOT; ?>/assets/images/profile.png" alt="Default Icon" id="profile-image">
+            <div class="overlay">
+                <span>Change Photo</span>
+            </div>
+        </label>
+        <input type="file" id="image-upload" accept="image/*">
+        <button id="delete-image">Delete Image</button>
+    </div>
+        <ul>
+            <li><a href="<?php echo URLROOT; ?>/Sellermarketplace"><img src="<?php echo URLROOT; ?>/assets/images/store.png"  alt="" class="auction">  Marketplace</a></li>
+            <li><a href="<?php echo URLROOT; ?>/Sellerauction"><img src="<?php echo URLROOT; ?>/assets/images/auction.png"  alt="" class="auction">  Auction</a></li>
+            <li><a href="<?php echo URLROOT; ?>/SellerAdrequest"><img src="<?php echo URLROOT; ?>/assets/images/ads.png"  alt="" class="auction">  Advertistements</a></li>
+            <li><a href=""><img src="<?php echo URLROOT; ?>/assets/images/orders.png"  alt="" class="auction">  Orders</a></li>
+            </ul>
+        </div>
