36eff008f76b31f08d2458b88933f23aa9ebefca
Nipul Yansith <nipulyansith26@gmail.com>
2023-10-17 23:34:11 +0530

    Delivery - Available Orders Page

---
+++ app/views/layouts/deliverySidebarWithoutImg.php
@@ -0,0 +1,12 @@
+<div class="Sellersidebar">
+    <div class="side_cont">
+            <ul>
+            <li><a href="<?php echo URLROOT; ?>/Sellermarketplace"><img src="<?php echo URLROOT ;?>/assets/images/store.png"  alt="" class="auction">  Available Orders</a></li>
+            <li><a href="<?php echo URLROOT; ?>/Sellerauction"><img src="<?php echo URLROOT ;?>/assets/images/orders.png"  alt="" class="auction">  Completed Orders</a></li>
+            <li><a href="<?php echo URLROOT; ?>/SellerAdrequest"><img src="<?php echo URLROOT; ?>/assets/images/review.png"  alt="" class="auction">  Reviews and Ratings</a></li>
+            <li><a href="<?php echo URLROOT; ?>/SellerOrder"><img src="<?php echo URLROOT ;?>/assets/images/orders.png"  alt="" class="auction">  Insight</a></li>        
+            </ul>
+    </div>
+    </div>  
+
+    
