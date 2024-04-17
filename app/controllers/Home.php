925f05933ad31ad706ec862f2578b88f2e80139f
Sonal Induwara <sonalinduwara@gmail.com>
2024-04-17 09:00:14 -0300

    small bugs

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -5,54 +5,17 @@
     private $adminModel;
     private $deliverModel;
     private $userModel;
+    private $orderModel;
    public function __construct()
    {
 
     $this->orderModel=$this->model("Order");
     $this->deliverModel =$this->model('Deliver');
-   }
-   public function index()
-   {
-       $data = [
-           'logged' => isset($_SESSION['user_id']),
-           'username' => isset($_SESSION['user_id']) ? ($_SESSION['user_name']) : null,
-       ];
-   
-       
-       if (isset($_SESSION['user_type']) && $_SESSION['user_type'] == 'deliver') {
-        
-            $deliver_id = $_SESSION['deliver_id'];
-            $details = $this->orderModel->getOngoingOrderDetails($deliver_id);
-            $rowB = $this->orderModel->getBuyerDetailsOngoingOrder($deliver_id);
-            $rowS = $this->orderModel->getSellerDetailsOngoingOrder($deliver_id);
-            $view;
-
-        $data = [
-            'details' => $details,
-            'rowB' => $rowB,
-            'rowS' => $rowS
-        ];
-
-        // if(!$details){
-        //     $view = false;
-        // }else{
-        //     $view = true;
-        // }
-
-
-
-
-
-        $this -> view('deliveryHome',$data);
-       } else {
-           // Load default home view
-           $this->view('home', $data);
-
     $this->adminModel =$this->model('Admin');
-     $this->deliverModel =$this->model('Deliver');
-     $this->userModel =$this->model('User');
-
+    $this->deliverModel =$this->model('Deliver');
+    $this->userModel =$this->model('User');
    }
+  
    public function index(){
 
     if(isset($_SESSION['user_type']) && $_SESSION['user_type']=='admin'){
@@ -73,9 +36,17 @@
   $this -> view('adminDash',$data);
   }
    elseif(isset($_SESSION['user_type']) && $_SESSION['user_type'] == 'deliver') {
-           // Assuming getOngoingOrdersdetails is a method in the Deliver model
-           $orders = $this->deliverModel->getOngoingOrdersDetails($_SESSION['user_id']);
-           $data['orders'] = $orders; // pass orders data to view
+    $deliver_id = $_SESSION['deliver_id'];
+    $details = $this->orderModel->getOngoingOrderDetails($deliver_id);
+    $rowB = $this->orderModel->getBuyerDetailsOngoingOrder($deliver_id);
+    $rowS = $this->orderModel->getSellerDetailsOngoingOrder($deliver_id);
+    $view;
+
+$data = [
+    'details' => $details,
+    'rowB' => $rowB,
+    'rowS' => $rowS
+];
            $this->view('deliveryHome', $data);
        } 
        else{
